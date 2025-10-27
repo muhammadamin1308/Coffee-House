7ff94fae35d1fefca7150e714d406654dde73b24
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2025-10-27 23:04:40 +0500

    test

--- coffee-house/src/config/api.ts
+++ coffee-house/src/config/api.ts
@@ -1,9 +1,6 @@
 const BACKEND_URL = 'http://coffee-shop-be.eu-central-1.elasticbeanstalk.com';
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com';
 
-export const API_BASE_URL = import.meta.env.PROD 
-  ? `${CORS_PROXY}/${BACKEND_URL}`
-  : BACKEND_URL;
+export const API_BASE_URL = BACKEND_URL;
 
 export const API_ENDPOINTS = {
   PRODUCTS: `${API_BASE_URL}/products`,
