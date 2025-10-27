4ee9b43c06d65d5c9113919a441b5c8f67dc8b95
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2025-10-27 14:59:23 +0500

    Fix: use  variable like working e-commerce project

--- coffee-house/vite.config.ts
+++ coffee-house/vite.config.ts
@@ -4,14 +4,11 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
+    assetsDir: 'assets'
   },
-  publicDir: 'public'
+  publicDir: 'public',
+  preview: {
+    host: '0.0.0.0',
+    strictPort: false
+  }
 })
